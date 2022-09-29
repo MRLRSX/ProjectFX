2df63b8bf324d7080251e704471a7f221d6f8766
Lucas Rocha <lucas.rocha-dev@outlook.com>
2022-09-29 15:31:09 -0300

    Classe seller

--- src/model/entidades/Seller.java
+++ src/model/entidades/Seller.java
@@ -1,9 +1,14 @@
 package model.entidades;
 
+import java.io.Serializable;
 import java.time.LocalDate;
+import java.util.Objects;
 
-public class Seller {
+public class Seller implements Serializable{
 
+	
+	private static final long serialVersionUID = 1L;
+	
 	private Long id;
 	private String name;
 	private String email;
@@ -72,6 +77,25 @@ public class Seller {
 	@Override
 	public String toString() {
 		return name ;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(baseSalary, birthDate, department, email, id, name);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Seller other = (Seller) obj;
+		return Objects.equals(baseSalary, other.baseSalary) && Objects.equals(birthDate, other.birthDate)
+				&& Objects.equals(department, other.department) && Objects.equals(email, other.email)
+				&& Objects.equals(id, other.id) && Objects.equals(name, other.name);
 	}
     
 	
