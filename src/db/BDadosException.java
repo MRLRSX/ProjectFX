5fa5af611cccac145c81a82f021a9908d644f3f3
Lucas Rocha <lucas.rocha-dev@outlook.com>
2022-09-29 14:57:19 -0300

    Classe BDadosException

---
+++ src/db/BDadosException.java
@@ -0,0 +1,10 @@
+package db;
+
+public class BDadosException extends RuntimeException {
+
+	private static final long serialVersionUID = -591331261493602064L;
+
+    public BDadosException (String mensagem) {
+    	super(mensagem);
+    }	
+}
