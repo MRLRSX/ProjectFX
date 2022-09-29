3265811424d14b8b60abca30eb6c5121880edde3
Lucas Rocha <lucas.rocha-dev@outlook.com>
2022-09-29 15:35:06 -0300

    Interface SellerDataAcessObject

---
+++ src/model/dao/SellerDataAcessObject.java
@@ -0,0 +1,14 @@
+package model.dao;
+
+import java.util.List;
+
+import model.entidades.Seller;
+
+public interface SellerDataAcessObject {
+  
+	public void insert(Seller seller);
+	public void update(Seller seller);
+	public void deleteById(Long id);
+	public Seller findById(Long id);
+	public List<Seller> findAll();
+}
